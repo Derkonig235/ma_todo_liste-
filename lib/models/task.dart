1862f43cb99b9c35a09b6237d5eb0a614fd31a93
Ouaziri Junior <ouazirijunior641@gmail.com>
2026-04-09 11:29:21 +0100

    Mise à jour du task, ajout de nouveaux éléments tels que status, priority, categogy, les dates et le reminder pour gérer les notifs

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,15 +1,44 @@
+enum TaskStatus {
+  inProgress,
+  done,
+  late,
+  abandoned,
+}
+
+enum TaskPriority {
+  high,
+  medium,
+  low,
+}
+
+enum TaskCategory {
+  personal,
+  work,
+  other,
+}
+
 class Task {
   String id;
   String title;
   String description;
-  bool isDone;
+  TaskStatus status;
+  TaskPriority priority;
+  TaskCategory category;
   DateTime createdAt;
+  DateTime startDate;
+  DateTime dueDate;
+  DateTime? reminder;
 
   Task({
     required this.id,
     required this.title,
     required this.description,
-    this.isDone = false,
+    this.status = TaskStatus.inProgress,
+    this.priority = TaskPriority.medium,
+    this.category = TaskCategory.personal,
     required this.createdAt,
+    required this.startDate,
+    required this.dueDate,
+    this.reminder,
   });
 }
