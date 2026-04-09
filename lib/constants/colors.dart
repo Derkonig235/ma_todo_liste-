18f0f8558e827c7893a04ed637cee1115365ce22
Ouaziri Junior <ouazirijunior641@gmail.com>
2026-04-09 11:30:34 +0100

    Changement de la couleur bleue foncée en bleue ciel

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF6200EE);
+  static const Color primary = Colors.blue;
   static const Color background = Color(0xFFF5F5F5);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
